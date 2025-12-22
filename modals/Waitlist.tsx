959f4b02215efb5b171e8457313e06ba0fd806df
Danish Khan <dk831675@gmail.com>
2025-12-22 21:17:58 +0530

    Fields added to Waitlist form

--- modals/Waitlist.tsx
+++ modals/Waitlist.tsx
@@ -9,16 +9,22 @@ import { toast } from "sonner"
 import { z } from "zod"
 
 interface WaitlistProps {
-  onClose: () => void;
+    onClose: () => void;
 }
 
+const fullNameSchema = z.string().min(1, "Full name is required").max(20, "At max 20 characters are allowed")
 const emailSchema = z.string().email("Please enter a valid email address").min(1, "Email is required");
+const occupationSchema = z.string().min(1, "Occupation is required")
 
 const Waitlist: React.FC<WaitlistProps> = ({ onClose }) => {
     const [mounted, setMounted] = useState(false);
     const [email, setEmail] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [occupation, setOccupation] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+    const [isFullNameError, setIsFullNameError] = useState(false);
+    const [isEmailError, setIsEmailError] = useState(false);
+    const [isOccupationError, setIsOccupationError] = useState(false);
 
     useEffect(() => {
         setMounted(true);
@@ -36,14 +42,30 @@ const Waitlist: React.FC<WaitlistProps> = ({ onClose }) => {
 
     const handelFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setIsError(false);
+        setIsFullNameError(false);
+        setIsEmailError(false);
+        setIsOccupationError(false);
 
-        const result = emailSchema.safeParse(email);
-        
-        if (!result.success) {
-            setIsError(true);
-            const errorMessage = result.error.issues[0]?.message || "Invalid email";
-            toast.error(errorMessage);
+        const fullNameResult = fullNameSchema.safeParse(fullName);
+        const emailResult = emailSchema.safeParse(email);
+        const occupationResult = occupationSchema.safeParse(occupation);
+
+        const errors = [];
+        if (!fullNameResult.success) {
+            setIsFullNameError(true);
+            errors.push(fullNameResult.error.issues[0]?.message || "Invalid full name");
+        }
+        if (!emailResult.success) {
+            setIsEmailError(true);
+            errors.push(emailResult.error.issues[0]?.message || "Invalid email");
+        }
+        if (!occupationResult.success) {
+            setIsOccupationError(true);
+            errors.push(occupationResult.error.issues[0]?.message || "Invalid occupation");
+        }
+
+        if (errors.length > 0) {
+            toast.error(errors[0]);
             return;
         }
 
@@ -53,10 +75,11 @@ const Waitlist: React.FC<WaitlistProps> = ({ onClose }) => {
             await new Promise((resolve) => setTimeout(resolve, 2000));
             toast.success("Successfully joined the waitlist!");
             setEmail("");
+            setFullName("");
+            setOccupation("");
             onClose();
         } catch (error) {
             toast.error("Something went wrong. Please try again.");
-            setIsError(true);
         } finally {
             setIsLoading(false);
         }
@@ -74,11 +97,11 @@ const Waitlist: React.FC<WaitlistProps> = ({ onClose }) => {
             >
 
                 {/* Close button */}
-                <div 
+                <div
                     className='absolute -top-5 right-0 bg-linear-to-bl from-red-700 to-red-500 p-2 rounded-full border-white/5 cursor-pointer group'
                     onClick={onClose}
                 >
-                    <X className='text-neutral-100 h-7 w-7 animate-pulse'/>
+                    <X className='text-neutral-100 h-7 w-7 animate-pulse' />
                 </div>
 
                 <div className='px-10 py-2 rounded-full bg-white/10 backdrop-blur-sm border-2 border-white/5'>
@@ -92,21 +115,48 @@ const Waitlist: React.FC<WaitlistProps> = ({ onClose }) => {
                     Join the waitlist to stay updated.
                 </p>
 
-                <Input
-                    value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                        if (isError) setIsError(false);
-                    }}
-                    type='email'
-                    placeholder='Enter your email'
-                    className={`mt-4.5 text-neutral-300 px-6 text-sm md:text-base lg:text-lg bg-[#2c2c2c] outline-none border-none transition-colors ${isError ? 'ring-1 ring-red-500' : ''}`}
-                />
+                <div className='w-full flex flex-col gap-1.5'>
+                    {/* Full Name */}
+                    <Input
+                        value={fullName}
+                        onChange={(e) => {
+                            setFullName(e.target.value);
+                            if (isFullNameError) setIsFullNameError(false);
+                        }}
+                        type='text'
+                        placeholder='Enter full name*'
+                        className={`mt-4.5 text-neutral-300 px-6 text-sm md:text-base lg:text-lg bg-[#2c2c2c] outline-none border-none transition-colors ${isFullNameError ? 'ring-1 ring-red-500' : ''}`}
+                    />
 
-                <Button 
+                    {/* Email */}
+                    <Input
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                            if (isEmailError) setIsEmailError(false);
+                        }}
+                        type='email'
+                        placeholder='Enter your email*'
+                        className={`mt-4.5 text-neutral-300 px-6 text-sm md:text-base lg:text-lg bg-[#2c2c2c] outline-none border-none transition-colors ${isEmailError ? 'ring-1 ring-red-500' : ''}`}
+                    />
+
+                    {/* Occupation */}
+                    <Input
+                        value={occupation}
+                        onChange={(e) => {
+                            setOccupation(e.target.value);
+                            if (isOccupationError) setIsOccupationError(false);
+                        }}
+                        type='text'
+                        placeholder='Enter your occupation*'
+                        className={`mt-4.5 text-neutral-300 px-6 text-sm md:text-base lg:text-lg bg-[#2c2c2c] outline-none border-none transition-colors ${isOccupationError ? 'ring-1 ring-red-500' : ''}`}
+                    />
+                </div>
+
+                <Button
                     disabled={isLoading}
-                    variant={'default'} 
-                    type='submit' 
+                    variant={'default'}
+                    type='submit'
                     className='text-xs md:text-sm lg:text-lg rounded-full cursor-pointer bg-linear-to-r from-brandOrange to-brandBlue transition-all ease-in-out duration-200 hover:scale-105 mt-8 px-10 h-11'
                 >
                     {isLoading ? (
